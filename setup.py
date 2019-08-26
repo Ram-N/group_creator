8a6145a77631a6102254b60e608bf67e7afcc42c
Ram-N <ramnarasimha@gmail.com>
2019-08-26 14:46:03 -0400

    Add full directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A python utility to create random equitable groups',
+    author='Ram Narasimhan',
+    license='MIT',
+)
